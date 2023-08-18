94c913e62d1618f7f27614e61eb096f26e93d66c
ahmed149 <abakhit149@gmail.com>
2023-08-17 23:22:53 -0700

    _

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * isalpha - checks if character is a letter
+ * _iisalpha - checks if character is a letter
  * both lowercase or uppercase
  *
  * @c: takes input from other functions.
