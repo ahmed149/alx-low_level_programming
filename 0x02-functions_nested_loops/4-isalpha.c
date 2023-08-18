e5a92d73685e97c21b3559efa5aa607c576f3747
ahmed149 <abakhit149@gmail.com>
2023-08-17 23:20:53 -0700

    _

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
  * isalpha - checks if character is a letter
  * both lowercase or uppercase
  *
- * 0c: takes input from other functions.
+ * @c: takes input from other functions.
  *
  * Return: 1 is c if true else 0
 */
