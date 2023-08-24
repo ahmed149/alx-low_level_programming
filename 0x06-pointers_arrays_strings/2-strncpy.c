fdf39e1030237eee69e5f099b5ed1c292cf6a8de
ahmed149 <abakhit149@gmail.com>
2023-08-24 10:28:26 -0700

    _

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,21 +16,21 @@ char *_strncpy(char *dest, char *src, int n)
 
 	/**
 	 * iterate through src array
-	 * where if  there is no null byte
-	 * among the first n bytes of source
-	 * the string palced in dest will not be
+	 * where if there is no null byte
+	 * mong the first n bytes of source
+	 * the string plced in dest will not be
 	 * null terminated
 	*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	/**
 	 * if the length of source is less than n
-	 * write additional nullbytes to dest to
+	 * write additional null bytes to dest to
 	 * ensure that a total of n bytes is written
 	*/
 	while (i < n)
-	{
-		dest[i] = '\0';
+{
+	dest[i] = '\0';
 		i++
 
 	return (dest);
