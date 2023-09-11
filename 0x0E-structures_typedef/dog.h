525c5626b52276cb002a7cfe97bfde6ad9980bcb
abakhit149 <abahit149@gmail.com>
2023-09-11 13:36:55 -0700

    _

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,13 @@
  *
  * Description: just a lone dog struct in a big kitty world
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
