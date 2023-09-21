d5fde908f043aaa8a4525b98828196fd83c8c668
ahmed149 <abakhit149@gmail.com>
2023-09-21 15:23:59 -0700

    _

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
+void _constructor(void) __attribute__ ((constructor));
+
 /**
- * priority - function that prints before main executes
+ * _constructor - executes before main()
  * Return: void
  */
-void __attribute__((constructor)) priority()
+void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
