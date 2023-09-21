066fd245963e94f2fc59f768590242fbdcf41ba2
ahmed149 <abakhit149@gmail.com>
2023-09-21 14:59:36 -0700

    _

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+
 #include <stdlib.h>
 
 /**
@@ -18,13 +20,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-int_strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void _constructor(void) __attribute__((constructor));
 
 #endif /* LISTS_H */
